96d326af211dfeeec8bc698fa12cbcedeb48da43
sato <sato.business.0117@gmail.com>
2026-09-02 06:03:44 +0900

    Use a stable flat ESLint configuration for provider payloads

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const { defineConfig, globalIgnores } = require("eslint/config");
+const vitalsModule = require("eslint-config-next/core-web-vitals");
+const typescriptModule = require("eslint-config-next/typescript");
+const nextVitals = vitalsModule.default || vitalsModule;
+const nextTypeScript = typescriptModule.default || typescriptModule;
+
+module.exports = defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+  globalIgnores([".next/**", "node_modules/**", "coverage/**", "next-env.d.ts"]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "react-hooks/set-state-in-effect": "off"
+    }
+  }
+]);
